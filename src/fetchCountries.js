5df641bdd908f1687bde2430a007d9cd20e02a83
Vlad Pazyna <vladpazyna@gmail.com>
2022-12-04 01:08:08 +0200

    updates

---
+++ src/fetchCountries.js
@@ -0,0 +1,9 @@
+const URL = 'https://restcountries.com/v3.1';
+
+function fetchCountries(name) {
+  return fetch(
+    `${URL}/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => response.json());
+}
+
+export default { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import country from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+let items = [];
 
 const refs = {
   searchInput: document.querySelector('#search-box'),
@@ -8,13 +12,48 @@ const refs = {
   countryInfo: document.querySelector('.countryInfo'),
 };
 
+const getListTemplate = ({ flags, name }) => `<li class="country-list__item">
+        <img src="${flags.png}" alt="${name.official}" width="26" />
+        <span class="country-list__text">${name.official}</span>
+      </li>`;
+
+const getItemTemplate = ({ flags, name, capital, population, languages }) => ``;
+
+const renderItem = () => {
+  refs.countryInfo.insertAdjacentElement('beforeend', markup);
+};
+
+const renderList = () => {
+  const list = items.map(getListTemplate);
+};
+
+const handleData = countries => {
+  console.log(countries);
+  if (countries.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  }
+
+  if (countries.length >= 2 && countries.length <= 10) {
+    const markup = countries.map(getListTemplate).join('');
+    console.log(markup);
+    refs.countryList.insertAdjacentHTML('beforeend', markup);
+  }
+
+  if (countries.length === 1) {
+  }
+};
+
 const handleInput = e => {
-  const { value } = e.currentTarget;
-  fetch(`https://restcountries.com/v3.1/name/${value}`)
-    .then(r => r.json())
-    .then(data => console.log(data))
+  const value = e.currentTarget.value.trim();
+  country
+    .fetchCountries(value)
+    .then(data => handleData(data))
     .catch(error => console.log(error));
   console.log(value);
 };
 
 refs.searchInput.addEventListener('input', handleInput);
+
+// debounce(handleInput, DEBOUNCE_DELAY);
